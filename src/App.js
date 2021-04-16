e969612de9d35ed5405b1f1da6f2a5ec4ced4f32
Jasmine Lane-Garica <jasminerlanegarcia@gmail.com>
2021-04-15 23:04:31 -0700

    Completed MVP

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header"
-
+import styled from "styled-components"
+import ReactPlayer from 'react-player'
 
 function App() {
 
   const [nasaData, setNasaData] = useState([])
   
   useEffect(() => {
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=vYUZ1oidnthI0koSRKn4fCybcfTGGI3OL11GvbK1")
     .then(res => {
+      console.log(res.data);
       setNasaData(res.data)
     })
     .catch(err => {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,29 @@
 import React from "react"
+import styled from "styled-components"
+import ReactPlayer from 'react-player'
+
+const BlueH1 = styled.h1 `
+  font-family: sans-serif;
+  color: #7F7FFF;
+`;
 
 const Header = (props) => {
+
 
   const { img } = props
 
     return (
         <header>
-          <h1>NASA POD</h1> 
-          <img src = {img.url} /> 
+          <BlueH1>NASA POD</BlueH1> 
+          {(img.media_type === 'video' ? 
+            <ReactPlayer
+              className='react-player fixed-bottom'
+              url= {img.url}
+              width='100%'
+              height='100%'
+              controls = {true}/> 
+            : 
+            <img src = {img.url} />)}
         </header>
     )
 }
